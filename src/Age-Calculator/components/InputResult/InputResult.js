d02cb98862c1fd22fa6510d15a85b84d0fee9149
unachoza <unachoza@gmail.com>
2023-09-25 17:33:37 -0400

    showing results correctly

--- src/Age-Calculator/components/InputResult/InputResult.js
+++ src/Age-Calculator/components/InputResult/InputResult.js
@@ -3,7 +3,7 @@ import "./InputResult.css"
 const InputResult = ({ result, label }) => {
     return (
         <div className="input-result">
-            <div className="day-result">-- </div>
+            <div className="day-result">{result === "" ? "--" : result}</div>
             {label}
         </div >
     )
